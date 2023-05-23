cdb3013d8cea864bb64d84eb6de58908ee3c80b0
Dhanus <dhanus3133@gmail.com>
2023-05-23 12:37:36 +0530

    null-ls rustfmt to edition 2021

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -1,27 +1,29 @@
-require('mason-null-ls').setup {
-  ensure_installed = { 'stylua', 'black', 'prettier', 'gofmt', 'clang-format', 'rustfmt', 'shfmt' },
-}
+require("mason-null-ls").setup({
+  ensure_installed = { "stylua", "black", "prettier", "gofmt", "clang-format", "rustfmt", "shfmt" },
+})
 
-local null_ls = require 'null-ls'
+local null_ls = require("null-ls")
 local blt = null_ls.builtins
 
-null_ls.setup {
+null_ls.setup({
   on_attach = on_attach,
   capabilities = capabilities,
   autostart = true,
   sources = {
-    blt.formatting.stylua.with {
-      extra_args = { '--config-path', vim.fs.normalize '~/.config/nvim/.stylua.toml' },
-    },
-    blt.formatting.black.with { args = { '--quiet', '-' } },
+    blt.formatting.stylua.with({
+      extra_args = { "--config-path", vim.fs.normalize("~/.config/nvim/.stylua.toml") },
+    }),
+    blt.formatting.black.with({ args = { "--quiet", "-" } }),
     blt.formatting.prettier,
     blt.formatting.gofmt,
     blt.formatting.clang_format,
-    blt.formatting.rustfmt,
+    blt.formatting.rustfmt.with({
+      extra_args = { "--edition=2021" },
+    }),
     blt.formatting.shfmt,
     -- blt.formatting.goimports,
     -- blt.formatting.gofumpt,
     -- blt.formatting.isort,
     -- blt.formatting.shfmt,
   },
-}
+})
