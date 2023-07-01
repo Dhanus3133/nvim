19976e75d098ad685ffb1a200cf571d97e04198a
Dhanus <dhanus3133@gmail.com>
2023-07-01 13:32:48 +0530

    feat: New plugins added
    - Octo.nvim
    - Diffview.nvim

--- after/plugin/autosave.lua
+++ after/plugin/autosave.lua
@@ -1,5 +1,19 @@
 require("auto-save").setup({
   enabled = true,
+  condition = function(buf)
+    local fn = vim.fn
+    local utils = require("auto-save.utils.data")
+    if fn.bufname(buf) == "lua/plugins.lua" then
+      return false
+    end
+    if fn.bufname(buf):find("octo://", 1, true) then
+      return false
+    end
+    if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), { "gitcommit" }) then
+      return true -- met condition(s), can save
+    end
+    return false -- can't save
+  end,
   -- execution_message = 'AutoSave: saved at ' .. vim.fn.strftime('%H:%M:%S'),
   events = { "InsertLeave", "TextChanged" },
   conditions = { exists = true, modifiable = true },
