8e5ada9cf3ad811ab79dea6f2cea085c72e641e5
Dhanus <dhanus3133@gmail.com>
2025-10-16 14:14:25 +0530

    feat: DAP for django project

--- lua/plugins/configs/dap.lua
+++ lua/plugins/configs/dap.lua
@@ -60,13 +60,24 @@ if has_mason then
   local debugpy = mason_registry.get_package("debugpy")
   local path = vim.fs.joinpath(vim.fn.expand("$MASON"), "packages", debugpy.name, "")
   require("dap-python").setup(path .. "venv/bin/python")
+  dap.configurations.python = {
+    {
+      type = "python",
+      request = "launch",
+      name = "Django Runserver",
+      program = "${workspaceFolder}/manage.py",
+      args = { "runserver", "--noreload" },
+      django = true,
+      justMyCode = false,
+    },
+  }
 end
 
 -- require("nvim-dap-virtual-text").setup({
 --   enabled = true, -- enable this plugin (the default)
 --   enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
-  -- highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
-  -- highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+-- highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
+-- highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
 --   show_stop_reason = true, -- show stop reason when stopped for exceptions
 --   commented = false, -- prefix virtual text with comment string
 --   only_first_definition = true, -- only show virtual text at first definition (if there are multiple)
